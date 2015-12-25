e72c62749a10aa72c02a9a1cc77d9334f450756c
osotn <sotnicov_alex@list.ru>
2015-12-25 23:06:26 +0200

    Baseline for Playlist for HandHelp of Travel App

---
+++ TravelApp/TravelPlaylistHandHelp/app/src/main/java/pasa/gl/com/travelplaylisthandhelp/Database.java
@@ -0,0 +1,19 @@
+// TODO Rename package name
+package pasa.gl.com.travelplaylisthandhelp;
+
+/**
+ * Created by oleksandr.v.sotnikov on 12/25/2015.
+ */
+public class Database {
+
+    // TODO Implement as Singletone. We need only one database for TravelApp
+
+    public Database() {
+
+    }
+
+    public boolean init() {
+        return true;
+    }
+
+}
