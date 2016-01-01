6c12687cc90f3069e1c9464123340da856a9c612
Oleksandr <sotnicov_alex@list.ru>
2016-01-01 18:27:27 +0200

    Baseline AndroidStudio2015 project

---
+++ TravelApp/TravelPlaylistHandHeld/app/src/main/java/com/gl/pasa/travelapp/PlaylistActivity.java
@@ -0,0 +1,13 @@
+package com.gl.pasa.travelapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlaylistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playlist_hand_held);
+    }
+}
